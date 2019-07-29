c66df70024284696f450d4d8103d900b25781d8f
Virtual Sense <virtual_sense@naver.com>
2019-07-29 18:01:02 +0900

    Update file_server1.py

--- socket/file_server1.py
+++ socket/file_server1.py
@@ -1,8 +1,13 @@
+#가장 간단한 파이썬 파일전송 서버
 import socket
 import sys
+
+#소켓을 만듭니다.
 s = socket.socket()
+#IP와 Port를 바인딩합니다.
 s.bind(("172.30.1.11",9999))
-s.listen(10) # Acepta hasta 10 conexiones entrantes.
+#접속을 기다립니다.
+s.listen(10) #10은 백로그(backlog)인데 accept()없이 대기 가능한 연결요청의 최대개수 입니다.
 
 while (True):
     sc, address = s.accept()
